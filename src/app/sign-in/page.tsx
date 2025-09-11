40b882783342b7dda8a475ba50e019d179349ac3
thidantas <eng.thiagohpdantas@gmail.com>
2025-09-11 16:20:34 -0300

    feat(auth): add server-side check in SignIn page to redirect authenticated users

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,7 +1,16 @@
 import Auth from 'templates/Auth'
 import FormSignIn from 'components/FormSignIn'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 
-export default function SignIn() {
+export default async function SignIn() {
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    redirect('/')
+  }
+
   return (
     <Auth title="Sign In">
       <FormSignIn />
