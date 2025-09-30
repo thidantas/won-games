31e254b07ced2704ab7eca68820c14ff8d12685a
thidantas <eng.thiagohpdantas@gmail.com>
2025-09-30 16:16:38 -0300

    feat(filters): create utility for ExploreSidebar and API filter adaptation using TDD

---
+++ src/utils/filters/test.ts
@@ -0,0 +1,55 @@
+import { ItemProps } from '@/components/ExploreSidebar'
+import { parseQueryStringToFilters, parseQueryStringToFilter } from '.'
+
+const filterItems: Pick<ItemProps, 'type' | 'name'>[] = [
+  { name: 'price_lte', type: 'radio' },
+  { name: 'platforms', type: 'checkbox' },
+  { name: 'developers', type: 'checkbox' },
+  { name: 'sort', type: 'radio' }
+]
+
+const queryString = {
+  price_lte: '100',
+  platforms: ['windows', 'linux'],
+  developers: 'Rockstar Games',
+  sort: 'price:desc'
+}
+
+describe('parsedQueryStringToFilters', () => {
+  it('should parse queryString to filters format', () => {
+    const parsedQuery = parseQueryStringToFilters({
+      queryString,
+      filterItems
+    })
+
+    expect(parsedQuery).toStrictEqual({
+      price: {
+        lte: 100
+      },
+      platforms: {
+        name: {
+          in: ['windows', 'linux']
+        }
+      },
+      developers: {
+        name: {
+          in: 'Rockstar Games'
+        }
+      }
+    })
+  })
+
+  it('should parse queryString to filters values format', () => {
+    const parsedQuery = parseQueryStringToFilter({
+      queryString,
+      filterItems
+    })
+
+    expect(parsedQuery).toStrictEqual({
+      price_lte: '100',
+      platforms: ['windows', 'linux'],
+      developers: ['Rockstar Games'],
+      sort: 'price:desc'
+    })
+  })
+})
